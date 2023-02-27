ca874fe90193598433448b2c2831395adf65eae5
Wojciech Nesteruk <wojciech.nesteruk@gmail.com>
2023-02-27 10:29:25 +0100

    7.8 Rock, paper, scissors

---
+++ kodilla-rps/src/main/java/com/kodilla/rps/Game.java
@@ -0,0 +1,65 @@
+package com.kodilla.rps;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Game {
+    private int computerMove;
+    private String computerAttribute;
+    private int playerMove;
+    private String playerAttribute;
+    public void playerTurn() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Wybierz swoje zagranie");
+        playerMove = scan.nextInt();
+        if (playerMove == 1) {
+            playerAttribute = "Kamień";
+        } else if (playerMove == 2) {
+            playerAttribute = "Papier";
+        } else if (playerMove == 3) {
+            playerAttribute = "Nożyce";
+        } else {
+            playerAttribute = "Wpisz cyfre 1 (kamień), 2 (papier) lub 3 (nożyce)";
+        }
+    }
+    public String getPlayerAttribute() {
+        return playerAttribute;
+    }
+    public void computerTurn() {
+        Random random = new Random(3);
+        computerMove = 0;
+        while (computerMove <= 0) {
+            computerMove = random.nextInt(3)+1;
+        }
+        if (computerMove == 1) {
+            computerAttribute = "Kamień";
+        } else if (computerMove == 2) {
+            computerAttribute = "Papier";
+        }else {
+            computerAttribute = "Nożyce";
+        }
+    }
+
+    public String getComputerAttribute() {
+        return computerAttribute;
+    }
+    public String gameRules() {
+        String gameResult = "";
+        if (playerMove == computerMove) {
+            gameResult = "Remis";
+        } else if (playerMove == 1 && computerMove == 2) {
+            gameResult = "Przegrałeś runde";
+        } else if (playerMove == 2 && computerMove == 3) {
+            gameResult = "Przegrałeś rundę";
+        } else  if (playerMove == 3 && computerMove == 1){
+            gameResult = "Przegrałeś rundę";
+        } else if (playerMove == 2 && computerMove == 1){
+            gameResult = "Wygrałeś rundę";
+        } else if (playerMove == 3 && computerMove == 2) {
+            gameResult = "Wygrałeś rundę";
+        } else if (playerMove == 1 && computerMove == 3) {
+            gameResult = "Wygrałeś rundę";
+        }
+        return gameResult;
+    }
+}
---
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -0,0 +1,65 @@
+package com.kodilla.rps;
+
+import java.util.Scanner;
+
+public class RpsRunner {
+    public static void main(String[] args) {
+        boolean end = false;
+        int quantityOfRoundsWon = 0;
+        int quantityOfRoundsLost = 0;
+        Game game = new Game();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Witaj w grze kamień, papier, nożyce!");
+        System.out.println("Podaj swoje imię.");
+        String playerName = scanner.nextLine();
+        System.out.println("Podaj do ilu wygranych rund ma toczyć się gra.");
+        int quantityOfRounds = scanner.nextInt();
+
+        System.out.println("Do gry będzie wykorzystywana klawiatura. \n" +
+                "Do zagrania kamienia wpisz: 1. \n" +
+                "Do zagrania papieru wpisz: 2. \n" +
+                "Do zagrania nożyc wpisz: 3. \n" +
+                "Jeśli chcesz zakończyć grę wpisz: x \n" +
+                "Jeśli chcesz zacząć grę od nowa wpisz: n");
+
+        while (!end) {
+            game.playerTurn();
+            String playerMove = game.getPlayerAttribute();
+            game.computerTurn();
+            String computerMove = game.getComputerAttribute();
+            System.out.println("Twój ruch to: " + playerMove +
+                    ". \n Ruch komputera to: " + computerMove);
+            String result = game.gameRules();
+            System.out.println(result);
+
+            if ("Wygrałeś rundę".equals(result)) {
+                quantityOfRoundsWon = quantityOfRoundsWon + 1;
+                if (quantityOfRoundsWon == quantityOfRounds) {
+                    System.out.println(playerName + " wygrałeś");
+                    System.out.println("Jeśli chcesz zacząć ponownie grę wpisz n");
+                    if (scanner.next().equals("n")) {
+                        System.out.println("Podaj do ilu zwycięstw chcesz grać");
+                        quantityOfRounds = scanner.nextInt();
+                        quantityOfRoundsWon = 0;
+                    } else if (scanner.next().equals("x")) {
+                        end = true;
+                    }
+                }
+            } else if ("Przegrałeś rundę".equals(result)) {
+                quantityOfRoundsLost = quantityOfRoundsLost + 1;
+                if (quantityOfRoundsLost == quantityOfRounds) {
+                    System.out.println(playerName + " niestety przegrałeś");
+                    System.out.println("Jeśli chcesz zacząć ponownie grę wpisz n");
+                    if (scanner.next().equals("n")) {
+                        System.out.println("Podaj do ilu zwycięstw chcesz grać");
+                        quantityOfRounds = scanner.nextInt();
+                        quantityOfRoundsWon = 0;
+                    } else if (scanner.next().equals("x")) {
+                        end = true;
+                    }
+                }
+            }
+        }
+    }
+}
