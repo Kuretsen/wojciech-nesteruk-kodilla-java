99211fcf13f9b359ec59615169176200d3ce54fe
Wojciech Nesteruk <wojciech.nesteruk@gmail.com>
2023-03-30 15:54:52 +0200

    13.1 Iteration through the titles using stream

--- kodilla-good-patterns/src/main/java/com/kodilla/Main.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/Main.java
@@ -4,6 +4,7 @@ import com.kodilla.good.patterns.challenges.MovieStore;
 
 public class Main {
     public static void main(String[] args) {
+        //13.1 good patterns challenges
         MovieStore movieStore = new MovieStore();
 
         System.out.println(movieStore.showMoviesTitles());
